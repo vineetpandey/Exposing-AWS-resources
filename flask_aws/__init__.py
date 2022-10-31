1bef7cacafa14e3dc83e0464bd6843f272efa591
Vineet Kumar Pandey <vineetpandeyalld@gmail.com>
2022-10-31 15:27:34 +0530

    updating the structure - django

--- flask_aws/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-#  A secret key is required to use CSRF protection.
-app.config['SECRET_KEY'] = 'c3485172fddc76f05434df6aa29a4e0d'
-
-from flask_aws import routes
--- run.py
+++
@@ -1,4 +0,0 @@
-from flask_aws import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
